42183635449aee10858af9879819c8726ead870c
Saran <saran26@gmail.com>
2026-08-11 14:16:20 +0530

    Initial project commit

---
+++ src/main/java/com/example/Sample.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Sample {
+    public static void main (String[] args)
+    {
+        String name ="vscode + maven";
+        System.out.println("Hello" + name);
+    }
+}
